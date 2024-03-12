1b06a8a8406ba1472ad231b66c8b9b878e749db4
DrPanayioths <panayioths0072019@gmail.com>
2024-03-13 00:05:14 +0200

    Settings Beta Minimize System

--- main.js
+++ main.js
@@ -27,3 +27,32 @@ function closesettings(){
    window.onload = redirectIfMobile();
    window.onload = isMobileDevice();
   
+
+
+
+
+  // Minimize On Start (Cookies Used)
+function check_minimize() {
+  localStorage.setItem("minimize","True");
+  var minimize = localStorage.getItem("minimize");
+  if (minimize === "True") {
+    document.getElementsByClassName("yconnector").style.width = "5px";
+    document.getElementsByClassName("yconnector").style.height = "5px";
+  } else if (minimize === "False") {
+    document.getElementById("videoshowergr").style.width = "600px";
+    document.getElementById("videoshowergr").style.height = "300px";
+  }
+}
+window.onload = check_minimize();
+
+function minimize_on() {
+  localStorage.setItem("minimize", "True");
+  document.getElementById("minimize_on").style.visibility = "hidden";
+  document.getElementById("minimize_off").style.visibility = "visible";
+}
+
+function minimize_off() {
+  localStorage.setItem("minimize", "False");
+  document.getElementById("minimize_on").style.visibility = "visible";
+  document.getElementById("minimize_off").style.visibility = "hidden";
+}
